2e61ead1a58835e7d51e739e35a0da823e5810a8
ikeharanaoya <jacsha21@gmail.com>
2020-11-30 16:23:14 +0900

    DB作成

---
+++ db/migrate/20201130065820_create_questions.rb
@@ -0,0 +1,22 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+
+      # 番号
+      t.integer :number,        null: false
+      # 本文
+      t.string :text,           null: false
+      # 区分
+      t.integer :division_id,   null: false
+      # 評価
+      t.integer :valuation,     null: false
+      # 反転項目
+      t.boolean :inversion,     null: false, default: false
+
+      # problems テーブル
+      t.references :problem,    null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
