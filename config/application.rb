4a66fb16f0b4513b839a70252b2ff9eb5f3958af
ikeharanaoya <jacsha21@gmail.com>
2020-12-02 19:12:54 +0900

    field_with_errorsを除外

--- config/application.rb
+++ config/application.rb
@@ -16,12 +16,7 @@ module PsychologicalTestApp
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_view.field_error_proc = Proc.new do |html_tag, instance|
-      if instance.kind_of?(ActionView::Helpers::Tags::Label)
-        # skip when label
-        html_tag.html_safe
-      else
-        html_tag.html_safe
-      end
+      %Q(#{html_tag}).html_safe
     end
   end
 end
