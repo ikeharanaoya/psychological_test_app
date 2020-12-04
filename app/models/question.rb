8794f2fdaf353faae048beecaaa99c0681e2b591
ikeharanaoya <jacsha21@gmail.com>
2020-12-04 18:54:50 +0900

    アソシエーション修正

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,5 +5,5 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   # division テーブル
-  belongs_to :division, dependent: :destroy
+  belongs_to :division, dependent: :destroy,primary_key: [:problem_id, :division_id], foreign_key: [:problem_id, :division_id]
 end
