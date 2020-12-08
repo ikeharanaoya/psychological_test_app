aa9acfd6ba46ea01d66aa46557c676a54650c3fe
ikeharanaoya <jacsha21@gmail.com>
2020-12-08 16:04:11 +0900

    コントローラー、ビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users
   # ルートパスの設定
   root to: "problems#index"
   # problemsのパス
   resources :problems, only: [:show,:create] do
     # usersのパス
-    resources :users, only: :index
+    resources :users, only: :show
   end
 end
