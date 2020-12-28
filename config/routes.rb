87a0a4f2e7ef2be01fd66e2cb37c73c2f343d992
ikeharanaoya <jacsha21@gmail.com>
2020-12-28 14:24:02 +0900

    registrationsコントローラーのルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # ルートパスの設定
   root to: "problems#index"
   # problemsのパス
