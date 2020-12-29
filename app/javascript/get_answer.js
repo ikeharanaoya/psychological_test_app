384f459fe443d6b0436dcdc69104b0c214b58519
ikeharanaoya <jacsha21@gmail.com>
2020-12-29 19:48:42 +0900

    賢者ボタン　色の変更処理追加

--- app/javascript/get_answer.js
+++ app/javascript/get_answer.js
@@ -6,33 +6,53 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
   // 心理テストのパス
   const path_problems = "problems"
+  const pathError = "/problems"
   // 0以上の整数のみ（ゼロ埋めなし）
   const paramsRegex = /^([1-9]\d*|0)$/
 
   // 心理テスト画面の場合
-  if(path.includes(path_problems) && paramsRegex.test(params)) {
+  if((path.includes(path_problems) && paramsRegex.test(params)) || path === pathError) {
     // 賢者ボタン
     const kenjaBtn = document.getElementById("kenja-btn");
-    kenjaBtn.addEventListener("click", (e) => { 
-      // フォーム送信処理をキャンセル
-      e.preventDefault();
-      // XMLHttpRequestを生成
-      const XHR = new XMLHttpRequest();
-      // XMLHttpRequestを初期化
-      XHR.open("GET", `${location.pathname}/kenja_search`, true);
-      // レスポンスの形式を定義
-      XHR.responseType = "json";
-      // 送信
-      XHR.send();
-      // 受信後の処理
-      XHR.onload = () => {
-        // エラー確認
-        if (XHR.status != 200) {
-          alert(`Error ${XHR.status}: ${XHR.statusText}`);
-          return null
-        }
-      };
-    });
+    if(kenjaBtn) {
+      kenjaBtn.addEventListener("click", (e) => { 
+        // フォーム送信処理をキャンセル
+        e.preventDefault();
+        // 問題のID取得
+        const problem_id = document.getElementById("problem_id");
+        // XMLHttpRequestを生成
+        const XHR = new XMLHttpRequest();
+        // XMLHttpRequestを初期化
+        XHR.open("GET", `/problems/${problem_id.value}/kenja_search`, true);
+        // レスポンスの形式を定義
+        XHR.responseType = "json";
+        // 送信
+        XHR.send();
+        // 受信後の処理
+        XHR.onload = () => {
+          // エラー確認
+          if (XHR.status != 200) {
+            alert(`Error ${XHR.status}: ${XHR.statusText}`);
+            return null
+          }
+
+          // 賢者の回答情報を取得
+          const answers = XHR.response.post;
+
+          // ラベル情報用の変数
+          let radioLabel
+          answers["answers"][0]
+          for (let i = 0; i < answers["answers"].length; i++) {
+            for (let j = 1; j <= answers["answers"][i]["answer"]; j++) {
+              // ラベル情報を取得
+              radioLabel = document.getElementById(`${i+1}_${j}`);
+              // 印をつける
+              radioLabel.style.background = "radial-gradient(ellipse closest-side at 50% 83%,rgb(9 6 148), rgb(220, 242, 253) 90%)"
+            }
+          }
+        };
+      });
+    }
 
     // 最新ボタン
     const newBtn = document.getElementById("new-btn");
