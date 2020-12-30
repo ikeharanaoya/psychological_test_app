8c0900cdef03c183e852007cb5b6b3df665a7bbd
ikeharanaoya <jacsha21@gmail.com>
2020-12-30 18:53:30 +0900

    最新ボタン　色の変更処理追加

--- app/javascript/get_answer.js
+++ app/javascript/get_answer.js
@@ -41,7 +41,6 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
           // ラベル情報用の変数
           let radioLabel
-          answers["answers"][0]
           for (let i = 0; i < answers["answers"].length; i++) {
             for (let j = 1; j <= answers["answers"][i]["answer"]; j++) {
               // ラベル情報を取得
@@ -76,6 +75,20 @@ window.addEventListener("DOMContentLoaded", ()=>{
           if (XHR.status != 200) {
             alert(`Error ${XHR.status}: ${XHR.statusText}`);
             return null
+          }
+
+          // 賢者の回答情報を取得
+          const answers = XHR.response.post;
+
+          // ラベル情報用の変数
+          let radioLabel
+          for (let i = 0; i < answers.length; i++) {
+            for (let j = 1; j <= answers[i]["answer"]; j++) {
+              // ラベル情報を取得
+              radioLabel = document.getElementById(`${i+1}_${j}`);
+              // 印をつける
+              radioLabel.style.background = "radial-gradient(ellipse closest-side at 50% 83%,rgb(255 190 0), rgb(220, 242, 253) 90%)"
+            }
           }
         }
       });
